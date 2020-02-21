8add39fe3604459bbf811378c1b397fa6e1276dc
SoHye Choi <wergreat10@naver.com>
2020-02-22 01:39:51 +0900

    20190220

--- BOJ/2798.py
+++ BOJ/2798.py
@@ -2,12 +2,17 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 numbers = list(map(int,sys.stdin.readline().split()))
-sums = []
-for i in numbers:
-    for j in numbers:
-        if i == j: continue
-        for k in numbers:
-            if j == k or i == k: continue
-            if (i+j+k)<=M: sums.append(i+j+k)
 
-print(max(sums))
+result = 0
+l = len(numbers)
+
+for i in range(l):
+    for j in range(i+1, l):
+        for k in range(j+1, l):
+            if (numbers[i]+numbers[j]+numbers[k]) == M:
+                print(M)
+                exit()
+            elif (numbers[i]+numbers[j]+numbers[k]) < M:
+                result = max(result, numbers[i]+numbers[j]+numbers[k])
+
+print(result)
